9b092ef74e1af4daf7770974bf23c0b65e906d38
Joan Esteban <joanestebanr@gmail.com>
2017-12-17 12:19:12 +0100

    integration for rotate

---
+++ integration_test/jerotate_test.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+source $(dirname $0)/cfg.sh
+
+SNAPSHOT=${JEBACKUP_BINS}/jebackup.sh
+JEROTATE=${JEBACKUP_BINS}/jerotate.sh
+DATASET=$(dirname $0)/dataset
+
+source ${JEBACKUP_LIBS}/ansicolors.sh
+source ${JEBACKUP_LIBS}/logger.sh
+
+
+function setUp(){
+	__TMP_FOLDER_BACKUPS=$(mktemp -d)
+	VERBOSE "setting up test: created empty folder to store backups: $__TMP_FOLDER_TO_STORE_BACK"
+}
+
+function tearDown(){
+	if [ ! -z $__TMP_FOLDER_BACKUPS ]; then
+		VERBOSE "tearing down test: erasing $__TMP_FOLDER_BACKUPS"
+		rm -Rf $__TMP_FOLDER_BACKUPS
+	fi
+}
+
+# $1 -> file
+# $2 -> size kb
+function inflate_file_size(){
+	local __FILE="$1"
+	local __KB="$2"
+	dd if=/dev/zero of=$__FILE bs=1024 count=$__KB 2>&1 | LOG_OUTPUT_FILTER  
+	
+	
+}
+
+# it simulate files without extra folders
+# simulate a dialy backup with a fresh backup made right now
+# 5 files of 3 Mb
+#[OUT  ] -rw-rw-r-- 1 zodiac zodiac 3,0M dic 13 11:13 backup-20171213-1513160021.tgz
+#[OUT  ] -rw-rw-r-- 1 zodiac zodiac 3,0M dic 14 11:13 backup-20171214-1513246421.tgz
+#[OUT  ] -rw-rw-r-- 1 zodiac zodiac 3,0M dic 15 11:13 backup-20171215-1513332821.tgz
+#[OUT  ] -rw-rw-r-- 1 zodiac zodiac 3,0M dic 16 11:13 backup-20171216-1513419221.tgz
+#[OUT  ] -rw-rw-r-- 1 zodiac zodiac 3,0M dic 17 11:13 backup-20171217-1513505621.tgz
+#[OUT  ] -rw-rw-r-- 1 zodiac zodiac 20K dic 17 11:13 master.incremental
+
+function inflate_false_backup_plain_folder(){
+	inflate_file_size $__TMP_FOLDER_BACKUPS/master.incremental 20
+	local __TSTAMP=$(date +%s)
+	for I in $(seq 1 5); do
+		local __FILE=$(date -d @${__TSTAMP} +backup-%Y%m%d-%s.tgz)
+		
+		inflate_file_size $__TMP_FOLDER_BACKUPS/${__FILE} 3000
+		touch -m -d "@${__TSTAMP}" $__TMP_FOLDER_BACKUPS/${__FILE}
+		__TSTAMP=$((__TSTAMP - 86400))
+		
+	done
+	ls -lah $__TMP_FOLDER_BACKUPS 2>&1 | LOG_OUTPUT_FILTER 
+	VERBOSE folder [$__TMP_FOLDER_BACKUPS] size $(du -hs $__TMP_FOLDER_BACKUPS )
+	
+}
+
+###############################################################################
+# TEST
+###############################################################################
+function DISABLED_test_fails_params() {
+	
+	 
+	$JEROTATE -k  
+	assertEquals $? 1
+}
+
+function test_maximum_size() {
+	inflate_false_backup_plain_folder
+	$JEROTATE  -d $__TMP_FOLDER_BACKUPS -m maximum_size -s ROTATE_MAXIMUM_SIZE_TARGET_MB=3 -s NO_REMOVE_FILES_NEWER_THRESHOLD_SECONDS=120 -v
+}
+
+set_logger_level $LOG_LEVEL_WARNING_CTE
+set_logger_level $LOG_LEVEL_VERBOSE_CTE
+source $SHUNIT2
+
